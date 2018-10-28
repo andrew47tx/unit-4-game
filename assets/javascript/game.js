04eabdbc9c265903b428fe60ab52d5f33b3598db
Andrew Robledo <andrewrobledo2@gmail.com>
2018-10-27 21:54:39 -0500

    added images and fucntions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,23 +3,62 @@ var randomResult;
 var lost;
 var win;
 
-randomResult= Math.floor(Math.random() * 69) + 30; // hoisting
+// setters
+// getters
+
+// $(".crystal").attr('class');
+
+var resetAndStart= function (){
+    $(".crystals").empty();
+    var images= [
+        'https://d1u5p3l4wpay3k.cloudfront.net/fortnite_gamepedia/7/70/Shadowshard_crystal_icon.png',
+        'https://www.rd.com/wp-content/uploads/2017/08/13_crystal_Reasons-Why-You-Should-Give-Crystals-a-Try_364248173_Abra-Cadabraaa-380x254.jpg', 
+        'http://atlas-content-cdn.pixelsquid.com/stock-images/natural-crystal-group-oJBXAX0-600.jpg', 
+        'http://www.crystalclearintentions.co.nz/wp-content/uploads/crystals.jpg'];
+
+    randomResult= Math.floor(Math.random() * 69) + 30; //hoisting
 
 
-$("#result").html("Random Result: " + randomResult);
-for(var i=0; i<4; i++){
+    $("#result").html("Random Result: " + randomResult);
+    for(var i=0; i<4; i++){
 
     var random= Math.floor(Math.random() * 11) +1;
     console.log(random);
 
     var crystal= $("<div>");
         crystal.attr({"class": "crystal", "data-random": random});
-
+        crystal.css({
+            "background-image":"url(" + images[i]) + " )",
+            "background-image":"url("
+        });
+        
     $(".crystals").append(crystal);
-    
+    }  
+    $("#previous").html("Total Score:" + previous);
 }
-$(".crystal").on("click", function () {
-    console.log($(this));
+
+resetAndStart();
+
+//event delegation
+$(document).on('click', "crystal", function() {
+    
+    var num= parseInt($(this).attr("data-random"));
+    previous += num;
+    $("#previous").html("Total Score:" + previous);
+    console.log(previous);
+
+    if(previous > randomResult){
+        lost++;
+        $("#lost").html("You Lost:" + lost);
+        previous= 0;
+        resetAndStart();
+    }
+    else if(previous === randomResult){
+        win++;
+        $("#win").html("You Win:" + win);
+        previous= 0;
+        resetAndStart();
+    }
 });
 
 // pseudo code
